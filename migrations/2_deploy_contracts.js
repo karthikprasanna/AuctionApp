f32502f85ccb734c04131ae0b456e9d10c259975
b30wulffz <shlokpandey123@gmail.com>
2021-10-10 14:00:18 +0530

    remove redundant contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const SimpleStorage = artifacts.require("./SimpleStorage.sol");
 const Auction = artifacts.require("./Auction.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+module.exports = function (deployer) {
   deployer.deploy(Auction);
 };
